90698a9a87481259d506aca9653a34032a92aa22
Tobias Peters <peterstobias2302@gmail.com>
2021-07-18 16:23:48 +0200

    Added Basic Authentication for the Backend Services

--- src/cart-service/src/main/java/com/whsgcashop/cart/CartServiceApplication.java
+++ src/cart-service/src/main/java/com/whsgcashop/cart/CartServiceApplication.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
